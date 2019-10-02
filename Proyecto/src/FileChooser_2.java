9a425d31dd905d9e66095b6bb865e560b2d72d2e
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-10-02 10:39:21 -0500

    Pruebas

---
+++ Proyecto/src/FileChooser_2.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
+public class FileChooser_2 extends Application {
+
+    // launch the application
+    public void start(Stage stage) {
+        System.out.println("sirve");
+    }
+    public static void main(String args[])
+    {
+
+        // launch the application
+        launch(args);
+    }
+}

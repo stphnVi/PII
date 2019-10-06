6bcc6c9ac019f6ac2d8773b378373df2e51d7f85
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-10-06 02:14:04 -0500

    Clase lista y Nodo listas (Simplemente enlazada)

---
+++ Text Reader/src/Nodo.java
@@ -0,0 +1,20 @@
+
+public class Nodo {
+    private Nodo siguiente;
+    private Object dato;
+
+    Nodo(Object dato){
+        this.dato = dato;
+        this.siguiente = null;
+    }
+    Nodo VerSig() {
+        return this.siguiente;
+    }
+    void agregar(Nodo n) {
+        this.siguiente = n;
+    }
+    Object VerDato() {
+        return this.dato;
+    }
+
+}

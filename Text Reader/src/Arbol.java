254e8c89846536d6bf3e6258296a4cd01fd6e87b
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-10-06 03:15:17 -0500

    Clase arbol,(insertar y recorrer), constiene su propia clase para crear nodos

---
+++ Text Reader/src/Arbol.java
@@ -0,0 +1,86 @@
+public class Arbol {
+    NodoArbol raiz;
+    public Arbol(){
+        raiz = null;
+    }
+
+    //método para insertar
+
+    /***
+     *
+     * @param i
+     * i es indice
+     * @param fruta
+     * i el nuevo nodo que insertaré
+     */
+    public void Insertar(int i, Object fruta){
+        NodoArbol n = new NodoArbol(i);
+        n.Contenido = fruta;
+        //Validar si la raiz es nula para crear el arbol
+
+        if(raiz == null){
+            raiz = n;
+
+            // si no se crea un nodo auxiliar para ver donde se coloca(izq/der)
+        }else{
+            NodoArbol aux = raiz;
+            //Ahora se escala dentro del arbol mientras el auxiliar no sea null
+
+            while (aux!=null){
+                n.Padre = aux;
+                if(n.llave >= aux.llave){
+                    aux = aux.Derecha;
+                }else{
+                    aux = aux.Izquierda;
+                }
+
+            }
+
+            //Ahora que ya se sabe donde debe ir el nodo, este se debe enlazar
+            if(n.llave< n.Padre.llave){
+                n.Padre.Izquierda =n;
+            }else{
+                n.Padre.Derecha = n;
+            }
+
+        }
+
+    }
+
+public void Recorrer(NodoArbol n){
+        //Validar
+        if(n!=null){
+            // Cambiar el orden de las recursiones para recorrer alrevez el arbol;
+            Recorrer(n.Izquierda);
+            System.out.println("indice: "+ n.llave+ "---" + "contenido: "+ n.Contenido);
+            Recorrer(n.Derecha);
+        }
+
+}
+
+
+
+    public class NodoArbol{
+        // Padre es el nodo dentro del arbol del cual podran nacer fruticas
+        public NodoArbol Padre;
+        // Direccion de la frutica
+        public NodoArbol Izquierda;
+        public NodoArbol Derecha;
+        // Indice que corresponde al nodo/frutica
+        public int llave;
+        //Contenido del nodo/frutica (object para poner cualquier tipo)
+        public Object Contenido;
+
+
+
+        //CONSTRUCTOR
+        public NodoArbol(int indice){
+            llave = indice;
+            Derecha = null;
+            Izquierda = null;
+            Padre = null;
+        }
+
+
+    }
+}
--- Text Reader/src/Main.java
+++ Text Reader/src/Main.java
@@ -1,9 +1,7 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -24,6 +22,27 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Proyecto II - Text Reader");
         primaryStage.show();
+
+        //Pruebas del arbol
+         Arbol arbol = new Arbol();
+        /***
+         * indice donde lo quiero poner y lo que quiero insertar/fruta
+         */
+        System.out.println("Observar el orden en el que se inserta: 1,2,8,4,0");
+        //OBSERVAR EL ORDEN EN QUE SE INSERTA Y EN QUE SE IMPRIME
+        arbol.Insertar(1,"Manzana");
+        arbol.Insertar(2,"Banano");
+        arbol.Insertar(8,"Pera");
+        arbol.Insertar(4,"Jocote");
+        arbol.Insertar(0,"Mango");
+
+
+        System.out.println("Todo el Arbol");
+        arbol.Recorrer(arbol.raiz);
+        System.out.println("parte Izquierda del Arbol(raiz no se lee)");
+        arbol.Recorrer(arbol.raiz.Izquierda);
+        System.out.println("parte Derecha del Arbol(raiz no se lee)");
+        arbol.Recorrer(arbol.raiz.Derecha);
 
 
 

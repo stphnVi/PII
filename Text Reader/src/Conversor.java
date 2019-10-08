68799f9d0f115d918955edd836a203354711fa7b
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-10-08 18:18:30 -0500

    nada nuevo

--- Text Reader/src/Conversor.java
+++ Text Reader/src/Conversor.java
@@ -16,14 +16,15 @@ public class Conversor {
         BufferedReader br = null;
 
         try {
-            // Apertura del fichero y creacion de BufferedReader para poder
-            // hacer una lectura comoda (disponer del metodo readLine()).
+            /*
+            Abrir el archivo y leerlo
+             */
+
             archivo = new File (a);
             fr = new FileReader (archivo);
             br = new BufferedReader(fr);
 
-            // Lectura del fichero
-            //String linea;
+
             String s2;
             String s1;
             s1 = br.readLine();
@@ -32,34 +33,28 @@ public class Conversor {
             int numTokens = 0;
             StringTokenizer st = new StringTokenizer(s1);
 
-            // bucle por todas las palabras
+            /*
+            separar las letras de la linea para poder insertar en el arbol
+             */
             while (st.hasMoreTokens())
             {
-
                 s2 = st.nextToken();
                 numTokens++;
                 arbol.Insertar(numTokens, s2);
-               // System.out.println ("    Palabra " + numTokens + " es: " + s2);
             }
-
-
-
-            /*
-            int i = 0;
-            while((linea=br.readLine())!=null)
-                arbol.Insertar(i,linea);
-                System.out.println(linea+ "--");
-                i++;
-
-             */
         }
         catch(Exception e){
             e.printStackTrace();
         }finally{
-            // En el finally cerramos el fichero, para asegurarnos
-            // que se cierra tanto si todo va bien como si salta
-            // una excepcion.
+            /*
+            cerrar el archivo e imprimir las frutas del arbol
+             */
+
             try{
+                System.out.println("parte Izquierda del Arbol(raiz no se lee)");
+                arbol.Recorrer(arbol.raiz.Izquierda);
+                System.out.println("parte Derecha del Arbol(raiz no se lee)");
+                arbol.Recorrer(arbol.raiz.Derecha);
 
                 System.out.println("Todo el Arbol");
                 arbol.Recorrer(arbol.raiz);
@@ -70,38 +65,5 @@ public class Conversor {
                 e2.printStackTrace();
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //introducir al arbol
-        /*
-
-        for(int i =0; i< a.length();i++){
-            b = (byte)a.charAt(i);
-
-            arbol.Insertar(b,a.charAt(i));
-
-        }
-        System.out.println("Todo el Arbol");
-        arbol.Recorrer(arbol.raiz);
-
-        System.out.println("parte Izquierda del Arbol(raiz no se lee)");
-        arbol.Recorrer(arbol.raiz.Izquierda);
-        System.out.println("parte Derecha del Arbol(raiz no se lee)");
-        arbol.Recorrer(arbol.raiz.Derecha);
-*/
     }
 }
